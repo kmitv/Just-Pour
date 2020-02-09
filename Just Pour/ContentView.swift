8b3e62f7280ca0ea4a267f924ce97acdd6aabf03
kmitv <wojtek.kmita@hotmail.com>
2020-02-09 11:41:48 +0100

    fixed navigation view responsiveness

--- Just Pour/ContentView.swift
+++ Just Pour/ContentView.swift
@@ -9,50 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+    
+    class pouroverMethod: Identifiable {
+        var name: String
+        var description: String
+        var picture: String
         
-
-        Section {
-            NavigationView {
-              
-                
-                    
-                    List {
-                        NavigationLink(destination: DetailView()) {
-                            Text("Detail")
-                        }
-                        NavigationLink(destination: DetailView()) {
-                            Text("Hello World")
-                        }
-                        NavigationLink(destination: DetailView()) {
-                            Text("Hello World")
-                        }
-                        NavigationLink(destination: DetailView()) {
-                            Text("Hello World")
-                        }
-                        NavigationLink(destination: Text("Detail View")) {
-                            Text("Hello World")
-                        }
-                    }
-                    .frame(minWidth: 100.00, maxWidth: 100.00, minHeight: 300.0, maxHeight: .infinity)
-                
-            }
-            
-            
-            }.frame(width: 300.00, alignment: .leading)
-
+        init(name: String, description: String, picture: String) {
+            self.name = name
+            self.description = description
+            self.picture = picture
         }
-
-    }
-
-struct DetailView: View {
-  let discipline = "ddddd"
-  var body: some View {
-    Section {
-        Text("pepepepe")
     }
     
-  }
+    let pouroverMethodList = [
+        pouroverMethod(name: "V60", description: "ememememe", picture:"jajajajajaj/sddsdsd"),
+        pouroverMethod(name: "Aeropress", description: "AEROPREEEES", picture:"jajajajajaj/sddsdsd"),
+        pouroverMethod(name: "Chemex", description: "TO WIELKI DZBAN", picture:"jajajajajaj/sddsdsd"),
+        pouroverMethod(name: "Gabi", description: "TO BRZMI JAK DAMSKIE IMIĘ", picture:"jajajajajaj/sddsdsd")
+    ]
+
+    var body: some View {
+        Section {
+            NavigationView {
+                List(pouroverMethodList) { method in
+                    NavigationLink(destination: DetailView(methodName: method.name, methodDescription: method.description, methodPicture: method.picture)) {
+                        Text(method.name)
+                    }
+                }
+                .frame(minWidth: 100.00, maxWidth: 100.00, minHeight: 300.0, maxHeight: .infinity)
+            }
+        }.frame(minWidth: 500.00, maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+    }
+}
+
+struct DetailView: View {
+    var methodName: String
+    var methodDescription: String
+    var methodPicture: String
+    
+    var body: some View {
+        Section {
+            Text(methodDescription)
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
